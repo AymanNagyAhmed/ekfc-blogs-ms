1bd0ef9f51d2cf74f77c231a490fc47bf518dc0c
AymanNagyAhmed <aymannagy.ahmed@gmail.com>
2024-12-21 23:55:27 +0400

    remove un used files

--- src/modules/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from '@/modules/auth/auth.service';
-import { AuthController } from '@/modules/auth/auth.controller';
-
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService],
-})
-export class AuthModule {}
